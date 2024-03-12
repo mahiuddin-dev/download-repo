57051223d43e6028d02ec27fdec7a7c85130e2b5
mahiuddun-dev <codermahiuddin@gmail.com>
2024-03-12 16:53:56 +0600

    fix pagination issue

--- repos.js
+++ repos.js
@@ -12,12 +12,102 @@ const getAccessToken = async () => {
     } catch (error) {
       console.error('Error fetching access token:', error);
     }
-  };
+};
+
+
+  function fetchRepos(apiUrl, github_accessToken, page, itemsPerPage, username, totalRepos) {
+    const start = (page - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    // fetch updated repos data desending order
+    fetch(`${apiUrl}?page=${page}&per_page=${itemsPerPage}&sort=updated&direction=desc`, {
+        headers: {
+            Authorization: `Bearer ${github_accessToken}`
+        }
+    })
+        .then(response => response.json())
+        .then(repos => {
+            displayRepos(repos, github_accessToken, username);
+            createPagination(apiUrl, totalRepos, itemsPerPage, page, github_accessToken, username, totalRepos);
+        })
+        .catch(error => console.error('Error fetching repositories:', error));
+}
+
+function displayRepos(repos, github_accessToken, username) {
+    const repoList = document.getElementById('repoList');
+    repoList.innerHTML = '';
+
+    repos.forEach(repo => {
+        getRepoDefaultBranch(repo, github_accessToken, username)
+            .then(defaultBranch => {
+                const listItem = document.createElement('li');
+                listItem.className = 'repo-item';
+                listItem.textContent = `${repo.name}`;
+
+                const downloadBtn = document.createElement('button');
+                downloadBtn.className = 'download-btn';
+                downloadBtn.textContent = 'Download Zip';
+                downloadBtn.addEventListener('click', () => downloadRepo(username, repo.name, defaultBranch));
+
+                listItem.appendChild(downloadBtn);
+                repoList.appendChild(listItem);
+            })
+            .catch(error => console.error('Error fetching default branch:', error));
+    });
+}
+
+function getRepoDefaultBranch(repo, github_accessToken, username) {
+    const apiUrl = `https://api.github.com/repos/${username}/${repo.name}`;
+
+    return fetch(apiUrl, {
+        headers: {
+            Authorization: `Bearer ${github_accessToken}`
+        }
+    })
+        .then(response => response.json())
+        .then(repoInfo => repoInfo.default_branch)
+        .catch(error => Promise.reject(error));
+}
+
+function downloadRepo(username, repoName, defaultBranch) {
+    window.open(`https://github.com/${username}/${repoName}/archive/refs/heads/${defaultBranch}.zip`);
+}
+
+function createPagination(reposApiUrl, totalItems, itemsPerPage, currentPage, github_accessToken, username, totalRepo) {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const paginationElement = document.getElementById('pagination');
+    paginationElement.innerHTML = '';
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageItem = document.createElement('li');
+        pageItem.className = `page-item ${i === currentPage ? 'active' : ''}`;
+        pageItem.innerHTML = `<a class="page-link">${i}</a>`;
+        paginationElement.appendChild(pageItem);
+    }
+
+    attachPaginationClickEvent(reposApiUrl, github_accessToken, itemsPerPage,  username, totalRepo);
+}
+
+
+function changePage(page, reposApiUrl, github_accessToken, itemsPerPage, username, totalRepos) {
+    currentPage = page;
+    fetchRepos(reposApiUrl, github_accessToken, currentPage, itemsPerPage, username, totalRepos);
+}
+
+
+function attachPaginationClickEvent(reposApiUrl, github_accessToken, itemsPerPage,  username, totalRepos) {
+    const paginationLinks = document.querySelectorAll('.page-link');
+    paginationLinks.forEach(link => {
+        link.onclick = function () {
+            const page = parseInt(this.textContent);
+            changePage(page, reposApiUrl, github_accessToken, itemsPerPage, username, totalRepos);
+        };
+    });
+}
 
 
   (async () => { 
     const github_accessToken = await getAccessToken();
-    console.log(github_accessToken);
+
     const username = 'abuanwar072'
     const userApiUrl = `https://api.github.com/users/${username}`
     const reposApiUrl = `https://api.github.com/users/${username}/repos`
@@ -35,84 +125,7 @@ const getAccessToken = async () => {
         .then(user => {
             totalRepos = user.public_repos;
             document.getElementById('repoCount').textContent = `Total Public Repositories: ${totalRepos}`;
-            fetchRepos(reposApiUrl, github_accessToken, currentPage);
+            fetchRepos(reposApiUrl, github_accessToken, currentPage, itemsPerPage, username, totalRepos);
         })
         .catch(error => console.error('Error fetching the user information:', error));
-
-    function fetchRepos(apiUrl, github_accessToken, page) {
-        const start = (page - 1) * itemsPerPage;
-        const end = start + itemsPerPage;
-        // fetch updated repos data desending order
-        fetch(`${apiUrl}?page=${page}&per_page=${itemsPerPage}&sort=updated&direction=desc`, {
-            headers: {
-                Authorization: `Bearer ${github_accessToken}`
-            }
-        })
-            .then(response => response.json())
-            .then(repos => {
-                displayRepos(repos);
-                createPagination(totalRepos, itemsPerPage, currentPage);
-            })
-            .catch(error => console.error('Error fetching repositories:', error));
-    }
-
-
-    function displayRepos(repos) {
-        const repoList = document.getElementById('repoList');
-        repoList.innerHTML = '';
-
-        repos.forEach(repo => {
-            getRepoDefaultBranch(repo)
-                .then(defaultBranch => {
-                    const listItem = document.createElement('li');
-                    listItem.className = 'repo-item';
-                    listItem.textContent = `${repo.name}`;
-
-                    const downloadBtn = document.createElement('button');
-                    downloadBtn.className = 'download-btn';
-                    downloadBtn.textContent = 'Download Zip';
-                    downloadBtn.addEventListener('click', () => downloadRepo(repo.name, defaultBranch));
-
-                    listItem.appendChild(downloadBtn);
-                    repoList.appendChild(listItem);
-                })
-                .catch(error => console.error('Error fetching default branch:', error));
-        });
-    }
-
-    function getRepoDefaultBranch(repo) {
-        const apiUrl = `https://api.github.com/repos/${username}/${repo.name}`;
-
-        return fetch(apiUrl, {
-            headers: {
-                Authorization: `Bearer ${github_accessToken}`
-            }
-        })
-            .then(response => response.json())
-            .then(repoInfo => repoInfo.default_branch)
-            .catch(error => Promise.reject(error));
-    }
-
-    function downloadRepo(repoName, defaultBranch) {
-        window.open(`https://github.com/${username}/${repoName}/archive/refs/heads/${defaultBranch}.zip`);
-    }
-
-    function createPagination(totalItems, itemsPerPage, currentPage) {
-        const totalPages = Math.ceil(totalItems / itemsPerPage);
-        const paginationElement = document.getElementById('pagination');
-        paginationElement.innerHTML = '';
-
-        for (let i = 1; i <= totalPages; i++) {
-            const pageItem = document.createElement('li');
-            pageItem.className = `page-item ${i === currentPage ? 'active' : ''}`;
-            pageItem.innerHTML = `<a class="page-link" onclick="changePage(${i})">${i}</a>`;
-            paginationElement.appendChild(pageItem);
-        }
-    }
-
-    function changePage(page) {
-        currentPage = page;
-        fetchReposWithPagination(reposApiUrl, github_accessToken, currentPage);
-    }
-
 })();
